969cb4ad1c94a487a6b07b235b87adb3600e669e
frasermcc9 <fraser@themccallums.net.nz>
2021-12-30 20:55:26 +1100

    Improve handling of default values

--- src/createStatefulContext.tsx
+++ src/createStatefulContext.tsx
@@ -2,18 +2,13 @@ import React from "react";
 
 type SetFunction<KeyType> = React.Dispatch<React.SetStateAction<KeyType>>;
 
-export function createStatefulContext<T extends {}>(defaults: T) {
-  const Context = React.createContext<[T, SetFunction<T>]>([
-    defaults,
-    () => {},
-  ]);
+export function createStatefulContext<T extends {}>(defaults: Omit<T, "children">) {
+  const Context = React.createContext<[Omit<T, "children">, SetFunction<Omit<T, "children">>]>([defaults, () => {}]);
 
-  const StatefulContextComponent: React.FC = ({ children }) => {
-    const [state, setState] = React.useState<T>(defaults);
+  const StatefulContextComponent: React.FC<Partial<T>> = ({ children, ...rest }) => {
+    const [state, setState] = React.useState<Omit<T, "children">>({ ...defaults, ...rest });
 
-    return (
-      <Context.Provider value={[state, setState]}>{children}</Context.Provider>
-    );
+    return <Context.Provider value={[state, setState]}>{children}</Context.Provider>;
   };
 
   return [() => React.useContext(Context), StatefulContextComponent] as const;
---
+++ test.js
@@ -0,0 +1,11 @@
+const a = {
+  x: 1,
+  y: 2,
+};
+
+const b = {
+  y: 10,
+  z: 20,
+};
+
+console.log({ ...a, ...b });
